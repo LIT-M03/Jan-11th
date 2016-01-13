76acbbe656f4431e5aeda66aec59642a74e38f11
Avrumi Friedman <info@lakewoodprogramming.com>
2016-01-13 11:18:20 -0500

    Added tons of javascripty things

--- MvcApplication2/public/test.js
+++ MvcApplication2/public/test.js
@@ -1,8 +1,27 @@
 ﻿$(function() {
-    $("form").on('submit', function () {
+    $("body").on('submit', 'form', function () {
         var form = $(this);
         var firstName = form.parent().parent().find("td:first").text();
         var confirmation = confirm("Are you sure you want to delete " + firstName + "?");
         return confirmation;
+    });
+
+
+    $("tr").popover({
+        html: true,
+        placement: 'right',
+        content: function () {
+            var td = $(this).find("td:eq(3)");
+            return td.html();
+        }
+    });
+
+    $("tr").on('shown.bs.popover', function() {
+        var current = this;
+        $("tr").each(function() {
+            if (this !== current) {
+                $(this).popover('hide');
+            }
+        });
     });
 });
